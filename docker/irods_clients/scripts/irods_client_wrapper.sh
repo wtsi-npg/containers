d3d4b81f75e72acabfad921635b6718d982e4e1e
Keith James <kdj@sanger.ac.uk>
2022-05-11 10:32:17 +0100

    Fix to avoid password echoing by iinit
    For iinit, run docker with "-t" to provide a TTY, so that it will not
    echo the password.

--- docker/irods_clients/scripts/irods_client_wrapper.sh
+++ docker/irods_clients/scripts/irods_client_wrapper.sh
@@ -8,17 +8,26 @@ set -eo pipefail
 
 IRODS_ENVIRONMENT_FILE=${IRODS_ENVIRONMENT_FILE:-"$HOME/.irods/irods_environment.json"}
 
-# The default container name matches that created by the start_client_container.sh script
+# The default container name matches that created by the start_client_container.sh
+# script
 DOCKER_CONTAINER=${DOCKER_CONTAINER:-"irods-clients"}
 CLIENT_USER_ID=${CLIENT_USER_ID:-$(id -u)}
 CLIENT_USER=${CLIENT_USER:-$USER}
 CLIENT_USER_HOME=${CLIENT_USER_HOME:-"$HOME"}
 
-docker exec -i \
+client=$(basename $0)
+
+# Provice a TTY for those applications that need it
+tty_arg=""
+if [ "$client" = "iinit" ]; then
+    tty_arg="-t"
+fi
+
+docker exec -i $tty_arg \
        -u $CLIENT_USER_ID \
        -w "$PWD" \
        -e CLIENT_USER_ID=$CLIENT_USER_ID \
        -e CLIENT_USER=$CLIENT_USER \
        -e CLIENT_USER_HOME="$CLIENT_USER_HOME" \
        -e IRODS_ENVIRONMENT_FILE="$IRODS_ENVIRONMENT_FILE" \
-       "$DOCKER_CONTAINER" "/opt/conda/envs/irods/bin/$(basename $0)" "$@"
+       "$DOCKER_CONTAINER" "/opt/conda/envs/irods/bin/$client" "$@"
